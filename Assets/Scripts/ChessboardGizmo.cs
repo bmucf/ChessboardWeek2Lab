e20ae54d914f07297d586dc0bf0f79955c8ecf9b
bmucf <br407069@ucf.edu>
2025-08-29 00:52:40 -0400

    added paths to show piece moves

--- Assets/Scripts/ChessboardGizmo.cs
+++ Assets/Scripts/ChessboardGizmo.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ChessboardGizmo : MonoBehaviour
 {
-    private Vector3 cubeDimensions = new Vector3(1,1,0);
+    private Vector3 cubeDimensions = new Vector3(1, 1, 0);
+    public Color color1 = new Color(1, 1, 1, 0.82f);
+    public Color color2 = new Color(0, 0, 0, 0.82f);
 
     /// <summary>
     /// The loop didn't work, so I had to write out a line to draw each square.
     /// </summary>
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.antiqueWhite;
-        Gizmos.DrawCube(new Vector3(-3.5f,3.5f,0), cubeDimensions);
+        Gizmos.color = color1;
+        Gizmos.DrawCube(new Vector3(-3.5f, 3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(-1.5f, 3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(0.5f, 3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(2.5f, 3.5f, 0), cubeDimensions);
@@ -43,7 +45,7 @@ public class ChessboardGizmo : MonoBehaviour
         Gizmos.DrawCube(new Vector3(1.5f, -3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(3.5f, -3.5f, 0), cubeDimensions);
 
-        Gizmos.color = Color.darkOliveGreen;
+        Gizmos.color = color2;
         Gizmos.DrawCube(new Vector3(-2.5f, 3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(-0.5f, 3.5f, 0), cubeDimensions);
         Gizmos.DrawCube(new Vector3(1.5f, 3.5f, 0), cubeDimensions);
--- Assets/Scripts/ChesspieceGizmo.cs
+++ Assets/Scripts/ChesspieceGizmo.cs
@@ -1,37 +1,151 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using static UnityEngine.Rendering.DebugUI;
 
-public enum Pieces
-{
-    Pawn,
-    Bishop,
-    Knight,
-    Rook,
-    Queen,
-    King
-}
-
 public class ChesspieceGizmo : MonoBehaviour
-
 {
+    public enum Pieces //creates dropdown menu in inspector
+    {
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen,
+        King
+    }
+
     public Pieces selectedPiece;
 
-    Dictionary<string, string> PieceIconSelect = new Dictionary<string, string>()
+    Dictionary<Pieces, string> PieceIconSelect = new Dictionary<Pieces, string>() //binds dropdown menu options to icon files
     {
-        {"Pawn", "Chess_plt60.png"},
-        {"Bishop", "Chess_blt60.png"},
-        {"Knight", "Chess_nlt60.png"},
-        {"Rook", "Chess_rlt60.png"},
-        {"Queen", "Chess_qlt60.png"},
-        {"King", "Chess_klt60.png"},
+        {Pieces.Pawn, "Chess_plt60.png"},
+        {Pieces.Bishop, "Chess_blt60.png"},
+        {Pieces.Knight, "Chess_nlt60.png"},
+        {Pieces.Rook, "Chess_rlt60.png"},
+        {Pieces.Queen, "Chess_qlt60.png"},
+        {Pieces.King, "Chess_klt60.png"},
     };
+
+    public Color iconColor = new Color(1f, 1f, 1f);
+    Vector3[] moves;
 
     void OnDrawGizmos()
     {
-        Gizmos.DrawIcon(transform.position, "{selectedPiece}", true);
-        Debug.Log(selectedPiece);
+
+
+        if (PieceIconSelect.TryGetValue(selectedPiece, out string iconName))
+        {
+            Gizmos.DrawIcon(transform.position, iconName, true, iconColor); //draws the chess pieces based on selected value from dropdown
+        }
+    }
+
+    private void OnDrawGizmosSelected() //draws a path for selected piece
+    {
+        switch (selectedPiece)
+        {
+            case Pieces.Pawn:
+                moves = new Vector3[2]
+                {
+                    transform.position,
+                    transform.position + new Vector3(0, 1, 0),
+                };
+                break;
+            case Pieces.Bishop:
+                moves = new Vector3[8]
+                {
+                    transform.position,
+                    transform.position + new Vector3(7, 7, 0),
+                    transform.position,
+                    transform.position + new Vector3(-7, -7, 0),
+                    transform.position,
+                    transform.position + new Vector3(7, -7, 0),
+                    transform.position,
+                    transform.position + new Vector3(-7, 7, 0),
+                };
+                break;
+            case Pieces.Knight:
+                moves = new Vector3[16]
+                {
+                    transform.position,
+                    transform.position + new Vector3(2, 1, 0),
+                    transform.position,
+                    transform.position + new Vector3(-2, -1, 0),
+                    transform.position,
+                    transform.position + new Vector3(2, -1, 0),
+                    transform.position,
+                    transform.position + new Vector3(-2, 1, 0),
+                    transform.position,
+                    transform.position + new Vector3(1, 2, 0),
+                    transform.position,
+                    transform.position + new Vector3(-1, -2, 0),
+                    transform.position,
+                    transform.position + new Vector3(-1, 2, 0),
+                    transform.position,
+                    transform.position + new Vector3(1, -2, 0),
+
+                };
+                break;
+            case Pieces.Rook:
+                moves = new Vector3[8]
+                {
+                    transform.position,
+                    transform.position + new Vector3(7, 0, 0),
+                    transform.position,
+                    transform.position + new Vector3(-7, 0, 0),
+                    transform.position,
+                    transform.position + new Vector3(0, 7, 0),
+                    transform.position,
+                    transform.position + new Vector3(0, -7, 0),
+                };
+                break;
+            case Pieces.Queen:
+                moves = new Vector3[16]
+                {
+                    transform.position,
+                    transform.position + new Vector3(7,7,0),
+                    transform.position,
+                    transform.position + new Vector3(-7,-7,0),
+                    transform.position,
+                    transform.position + new Vector3(7,-7,0),
+                    transform.position,
+                    transform.position + new Vector3(-7, 7, 0),
+                    transform.position,
+                    transform.position + new Vector3(7,0,0),
+                    transform.position,
+                    transform.position + new Vector3(-7,0, 0),
+                    transform.position,
+                    transform.position + new Vector3(0,7,0),
+                    transform.position,
+                    transform.position + new Vector3(0, -7, 0),
+                };
+                break;
+            case Pieces.King:
+                moves = new Vector3[16]
+                {
+                    transform.position,
+                    transform.position + new Vector3(1, 0, 0),
+                    transform.position,
+                    transform.position + new Vector3(-1, 0, 0),
+                    transform.position,
+                    transform.position + new Vector3(0, 1, 0),
+                    transform.position,
+                    transform.position + new Vector3(0, -1, 0),
+                    transform.position,
+                    transform.position + new Vector3(1, 1, 0),
+                    transform.position,
+                    transform.position + new Vector3(-1, -1, 0),
+                    transform.position,
+                    transform.position + new Vector3(1, -1, 0),
+                    transform.position,
+                    transform.position + new Vector3(-1, 1, 0),
+                };
+                break;
+        }
+        Gizmos.color = Color.white;
+        Gizmos.DrawLineList(moves);
     }
 }
